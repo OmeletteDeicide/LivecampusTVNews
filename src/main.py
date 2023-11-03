32573650fad88959c35a96c50f7f4063b650426e
flocorbab <51161916+flocorbab@users.noreply.github.com>
2023-11-04 00:50:32 +0100

    algo 2/2

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 
 from scrapper import get_all_episode
 from episodes_to_csv import save_episodes_to_csv
+from algo import infos_episode
+
+
+episodes = get_all_episode()
+infos = infos_episode(episodes)
+print(infos)
