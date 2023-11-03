27511eb8fd52a6e8e703784d4ee412d74ecfa127
Isosel26000 <yassine.ainane@livecampus.tech>
2023-11-03 16:51:16 +0100

    add csv

--- src/episodes_to_csv.py
+++ src/episodes_to_csv.py
@@ -1,20 +1,25 @@
 import csv
 
+from scrapper import get_all_episode
+
+
 def save_episodes_to_csv(episodes, csv_filename):
     with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=';')
 
         # Écrire l'en-tête du CSV
-        writer.writerow(["nom série", "numero de lepisode", "numero de la saison", "date diffusion", "origine", "plateform", "url episode"])
+        writer.writerow(["nom série", "numéro episode", "numéro saison", "date diffusion", "origine", "plateform", "url"])
 
         for episode in episodes:
             writer.writerow([
-                episode.get("nom_serie", ""),
-                episode.get("numero de lepisode", ""),
-                episode.get("numero de la saison", ""),
+                episode.get("nom série", ""),
+                episode.get("numéro episode", ""),
+                episode.get("numéro saison", ""),
                 episode.get("date diffusion", ""),
                 episode.get("origine", ""),
                 episode.get("plateform", ""),
-                episode.get("url episode", "")
+                episode.get("url", "")
             ])
+episodes = get_all_episode()
 
+save_episodes_to_csv(episodes, 'src/data/files/episodes.csv')
