7770c6071257009cbe3b25408cdd1512100754f6
flocorbab <51161916+flocorbab@users.noreply.github.com>
2023-11-03 21:01:55 +0100

    nouvelle fonction scrapper et ajout fichier algo

---
+++ src/algo.py
@@ -0,0 +1,83 @@
+def infos_episode(liste_episodes):
+    # création dictionnaire pour y inserer les infos
+    resultat =  {
+                "episodes_par_chaine":{},
+                "episodes_par_pays":{},
+                "mots_recurrents":{}
+                
+            }
+    # liste de chaque titre sans doublon
+    liste_titres=[]
+
+    # liste des mots dans les titres
+    liste_mots=[]
+
+    # boucle sur chaque episode
+    for episode in liste_episodes:
+        # si la chaine n'a pas encore été référencé on initialise la valeur à 1 sinon on incrémente de 1
+        if episode["chaine"] not in resultat["episodes_par_chaine"]:
+            resultat["episodes_par_chaine"][episode["chaine"]]=1
+        else:
+            resultat["episodes_par_chaine"][episode["chaine"]] += 1
+
+        # si le pays n'a pas encore été référencé on initialise la valeur à 1 sinon on incrémente de 1
+        if episode["origine"] not in resultat["episodes_par_pays"]:
+            resultat["episodes_par_pays"][episode["origine"]]=1
+        else:
+            resultat["episodes_par_pays"][episode["origine"]] += 1
+
+        # si le titre n'a pas encore été référencé on l'ajoute à la liste
+        if episode["nom série"] not in liste_titres:
+            liste_titres.append(episode["nom série"])
+    
+    # boucle sur tout les titres
+    for titre in liste_titres:
+
+        # boucle sur chaque mot de chque titre pour inserer le mot dans la liste "liste_mots" et mettre à jour dans le dictionnaire la récurrence du mot dans la liste "liste_mots"
+        for mot in titre.split(" "):
+            liste_mots.append(mot)
+            resultat["mots_recurrents"][mot]=liste_mots.count(mot)
+
+    # tri chaque dictionnaire dans l'ordre décroissant 
+    resultat["episodes_par_chaine"] =dict(sorted(resultat["episodes_par_chaine"].items(), key=lambda item: item[1], reverse=True))
+    resultat["episodes_par_pays"] =dict(sorted(resultat["episodes_par_pays"].items(), key=lambda item: item[1], reverse=True))
+    resultat["mots_recurrents"] =dict(sorted(resultat["mots_recurrents"].items(), key=lambda item: item[1], reverse=True))
+
+
+    return resultat
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+def group_list(lst):
+    x={}
+    for el in x:
+        x[el]=lst.count(el)
+    x =dict(sorted(x.items(), key=lambda item: item[1], reverse=True))
+            
+    return x
+     
+# Driver code
+lst = ["a", "a","b", "c", "d", "c", "c", "d"]
+print(group_list(lst))
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,24 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
-def get_all_episode():
+
+def get_all_episode(by_date=False,by_chaine=False,with_time=False):
+    # si la date n'est pas donné on prend la date actuel
+    if(by_date == False):
+        by_date = datetime.datetime.now()
+        by_date=by_date.strftime("%Y-%m")
+    # création dictionnaire pour y mettre chaque épisode
     all_episodes = []
     # on récupère le code html de l'url
-    url = "https://www.spin-off.fr/calendrier_des_series.html"
+    url = "https://www.spin-off.fr/calendrier_des_series.html?date="+by_date
     response = requests.get(url)
     content = response.content
     # On parse la page avec BeautifulSoup
     page = BeautifulSoup(content, 'html.parser')
+
+    # on récupère les td avec comme class "floatleftmobile"
     floatleftmobiles  = page.find_all('td',class_=['floatleftmobile'])
  
+    # boucle sur chaque td
     for floatleftmobile in floatleftmobiles:
+        
+        # si le td font partis du mois en court
         if(floatleftmobile.find("div",class_=['div_jour'])):
-            #jour
+
+            # on récupère la date dans l'id de la div avec la class "div_jour"
             jour = floatleftmobile.find("div",class_=['div_jour']).get("id").strip("jour_")
-            # selection des elements avec la class "calendrier_episodes" qui contient l'url de la page de l'épisode
+
+            # selection des elements avec la class "calendrier_episodes" qui contient les infos de l'épisode
             calendrier_episodes = floatleftmobile.find_all(class_="calendrier_episodes")
-            for index, calendrier_episode in enumerate(calendrier_episodes):
-                #origine
+
+            # boucle sur les élement avec la class "calendrier_episodes"
+            for calendrier_episode in calendrier_episodes:
+
+                # on obtient les infos a l'aide de find_previous_sibling(), split(), strip()...
                 origine = calendrier_episode.find_previous_sibling().find_previous_sibling().get("alt")
                 chaine = calendrier_episode.find_previous_sibling().get("alt")
                 str_infos = calendrier_episode.find_all("a")[1].get("title")
@@ -26,25 +43,40 @@ def get_all_episode():
                 name = str_infos.split("saison")[0].strip()
                 numero_saison = str_infos.split("saison")[1].split("episode")[0].strip()
                 numero_episode = str_infos.split("saison")[1].split("episode")[1].strip()
- 
+
+                # on stock les infos dans un dictionnaire
                 episode ={
                 "origine":origine,
                 "nom série":name,
                 "numéro saison":numero_saison,
                 "numéro episode":numero_episode,
-                "plateform":chaine,
+                "chaine":chaine,
                 "date diffusion":jour,
-                "url":url,
-                "durée": ""
+                "url":url
                 }
-                if(chaine=="Apple TV+"):
-                    response = requests.get(url)
-                    content = response.content
-                    page = BeautifulSoup(content, 'html.parser')
-                    
-                    #nom_episode = page.find('div',class_=['main_table']).find("meta", itemprop="name").get("content")
-                    duree_episode = page.find('div',class_=['episode_infos_episode_format']).text.replace("\n","").replace("\t","").strip()
-                    episode["durée"] = duree_episode
-                all_episodes.append(episode)
+
+                # si le fitre by_chaine est demandé
+                if(by_chaine != False):
+
+                    # la chaine de l'épisode correspond au filtre
+                    if(by_chaine==chaine):
+
+                        # si with_time est True on va chercher la durée de l'épisode seulement si le filtre by_chaine est activé sinon requêtes trop longue
+                        if(with_time != False):
+                            response = requests.get(url)
+                            content = response.content
+                            page = BeautifulSoup(content, 'html.parser')
+                            duree_episode = page.find('div',class_=['episode_infos_episode_format']).text.replace("\n","").replace("\t","").strip().split("minutes")[0]
+                            episode["durée"]=duree_episode
+                        # on ajoute l'épisode au dictionnaire
+                        all_episodes.append(episode)
+                else:
+                    # si aucun filtre et parametre et activé on ajoute l'épisode au dictionnaire sans la durée
+                    all_episodes.append(episode)
+
+    # return les épisodes
     return all_episodes
 
+
+
+#episodes = get_all_episode("2023-10","France 2",True)
