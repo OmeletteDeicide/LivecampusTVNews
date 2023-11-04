41b4a3476242e84df8cd47efb37800b2183b823d
flocorbab <51161916+flocorbab@users.noreply.github.com>
2023-11-04 11:48:41 +0100

    algo 2/2 scraping 2/2

--- src/main.py
+++ src/main.py
@@ -3,7 +3,20 @@ from scrapper import get_all_episode
 from episodes_to_csv import save_episodes_to_csv
 from algo import infos_episode
 
+# Scraping 1/2 
+episodes_octobre = get_all_episode("2023-10")
 
-episodes = get_all_episode()
-infos = infos_episode(episodes)
-print(infos)
+# Scraping 2/2 
+episodes_octobre_with_time = get_all_episode("2023-10","Apple tv+",True)
+
+# Algo
+infos = infos_episode(episodes_octobre)
+
+# Algo 1/2
+infos["episodes_par_chaine"]
+infos["episodes_par_pays"]
+infos["mots_recurrents"]
+
+# Algo 2/2
+infos["chaine_jour_consecutif_diffusion"]
+
